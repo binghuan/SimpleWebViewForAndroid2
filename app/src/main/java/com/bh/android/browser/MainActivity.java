d8cb307874a32636e6ce5b08baf91b0cb9a11850
binghuan <binghuanlin@gmail.com>
2017-04-06 16:47:05 +0800

    First check-in

---
+++ app/src/main/java/com/bh/android/browser/MainActivity.java
@@ -0,0 +1,65 @@
+package com.bh.android.browser;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView = null;
+    private EditText mUrlInputField = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.webview);
+        mUrlInputField = (EditText)findViewById(R.id.url_input);
+        mUrlInputField.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    //Toast.makeText(HelloFormStuff.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+                    mWebView.loadUrl(mUrlInputField.getText().toString());
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
+            CookieManager cookieMgr = CookieManager.getInstance();
+        cookieMgr.setAcceptCookie(true);
+        //cookieMgr.setAcceptThirdPartyCookies(mWebView, true);
+
+        //20161228@BH_Lin: Method to debug the WebView.
+        // Reference: https://developers.google.com/web/tools/chrome-devtools/remote-debugging/webviews
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mWebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.clearCache(true);
+        mWebView.clearHistory();
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        //mWebView.loadUrl("http://192.168.0.121:8080/index.html");
+    }
+}
