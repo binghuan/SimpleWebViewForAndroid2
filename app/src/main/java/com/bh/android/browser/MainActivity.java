9decf581369912a1fb21bab6112e0776769dc6ba
binghuan <binghuanlin@gmail.com>
2017-04-06 23:07:15 +0800

    Update icon

--- app/src/main/java/com/bh/android/browser/MainActivity.java
+++ app/src/main/java/com/bh/android/browser/MainActivity.java
@@ -1,46 +1,97 @@
 package com.bh.android.browser;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView = null;
     private EditText mUrlInputField = null;
+    private ProgressBar mProgressBar = null;
+    private ImageButton mButtonClearInput = null;
+
+    private final String TAG = "BH_Browser";
+
+    private final boolean DBG = true;
+
+    private void showLoadingProgress(boolean isVisible) {
+        if (isVisible) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private void loadUrl(View v) {
+
+
+        String targetUrl = mUrlInputField.getText().toString();
+        mWebView.loadUrl(targetUrl);
+        if (DBG) Log.v(TAG, "loadUrl: " + targetUrl);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        showLoadingProgress(true);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (DBG) Log.v(TAG, ">> onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView)findViewById(R.id.webview);
-        mUrlInputField = (EditText)findViewById(R.id.url_input);
-        mUrlInputField.setOnKeyListener(new View.OnKeyListener() {
+        mButtonClearInput = (ImageButton) findViewById(R.id.clearinput);
+        mButtonClearInput.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    // Perform action on key press
-                    //Toast.makeText(HelloFormStuff.this, edittext.getText(), Toast.LENGTH_SHORT).show();
-                    mWebView.loadUrl(mUrlInputField.getText().toString());
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            public void onClick(View v) {
+                mUrlInputField.setText("");
+            }
+        });
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mUrlInputField = (EditText) findViewById(R.id.url_input);
+        mUrlInputField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (DBG) Log.v(TAG, "onEditorAction: actionID = " + actionId);
+
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_SEND
+                        || actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_GO
+                        || actionId == EditorInfo.IME_ACTION_NEXT) {
+                    loadUrl(v);
+                    return true;
+                } else if (event != null && event.getAction() == KeyEvent.ACTION_DOWN
+                        && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    loadUrl(v);
                     return true;
                 }
+
                 return false;
             }
         });
-        
+
+
         CookieManager cookieMgr = CookieManager.getInstance();
         cookieMgr.setAcceptCookie(true);
         //cookieMgr.setAcceptThirdPartyCookies(mWebView, true);
@@ -52,7 +103,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                showLoadingProgress(false);
+                getSupportActionBar().setTitle(view.getTitle());
+            }
+        });
         mWebView.clearCache(true);
         mWebView.clearHistory();
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -61,5 +118,19 @@ public class MainActivity extends AppCompatActivity {
         }
         mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         //mWebView.loadUrl("http://192.168.0.121:8080/index.html");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (DBG) Log.v(TAG, ">> onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (DBG) Log.v(TAG, ">> onPause");
     }
 }
